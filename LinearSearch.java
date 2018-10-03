7d16a0d499c71a79c566fd4b804c40ba71b2ca52
sathiyaraj <sathiyaraj@192.168.0.127>
2018-10-04 02:16:24 +0530

    Binary search

--- LinearSearch.java
+++ LinearSearch.java
@@ -3,7 +3,14 @@ class LinearSearch {
 	public static void main(String args[]) {
         LinearSearch ls= new LinearSearch();
 		int arr[] = new int[]{34,4,65,76,65,57,44,77,31,7,8};
-		int n=57;
+		int n=578;
+		System.out.println("Binary Search iterative method");
+		int resultb=ls.binarySearch(arr,n);
+		if(resultb==-1)
+			System.out.println("Not found");
+		else
+			System.out.println("Found at:"+resultb);
+		System.out.println("Linear Search");
 		int result=ls.search(arr,n);
 		if(result==-1)
 			System.out.println("Not found");
@@ -11,6 +18,7 @@ class LinearSearch {
 			System.out.println("Found at:"+result);
 		
 	}
+	//Linear search n
 	public int search(int arr[],int n) {
 		for(int i=0;i<arr.length;i++){
 
@@ -19,6 +27,20 @@ class LinearSearch {
 		}
 		return -1;
 
+	}
+	//Binary search -Log n
+	public int binarySearch(int arr[],int n) {
+		int l=0,r=arr.length-1;
+		while(l<=r) {
+			int m=(l+r)/2;
+			if(arr[m]==n)
+				return m;
+			if(n<arr[m])
+				r=m-1;
+			if(n>arr[m])
+				l=m+1;
+		}
+		return -1;
 	}
 	
 }
